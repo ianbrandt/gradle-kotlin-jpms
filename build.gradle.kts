19676b83d1ec6246860701952c03c254795a7cc9
Ian Brandt <ian@ianbrandt.com>
2022-11-29 09:27:34 -0800

    Added code style configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    application
-    kotlin("jvm") version "1.7.21"
+	application
+	kotlin("jvm") version "1.7.21"
 }
 
 group = "com.ianbrandt"
@@ -13,49 +13,49 @@ val javaTargetVersion = JavaVersion.VERSION_17.toString()
 val kotlinTargetVersion = "1.7"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 kotlin {
-    jvmToolchain  {
-        languageVersion.set(JavaLanguageVersion.of(javaTargetVersion))
-    }
+	jvmToolchain {
+		languageVersion.set(JavaLanguageVersion.of(javaTargetVersion))
+	}
 }
 
 application {
-    mainModule.set("com.ianbrandt.jpms")
-    mainClass.set("com.ianbrandt.jpms.HelloWorldKt")
+	mainModule.set("com.ianbrandt.jpms")
+	mainClass.set("com.ianbrandt.jpms.HelloWorldKt")
 }
 
 tasks {
 
-    withType<JavaCompile>().configureEach {
-        sourceCompatibility = javaTargetVersion
-        targetCompatibility = javaTargetVersion
-    }
+	withType<JavaCompile>().configureEach {
+		sourceCompatibility = javaTargetVersion
+		targetCompatibility = javaTargetVersion
+	}
 
-    withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            languageVersion = kotlinTargetVersion
-            apiVersion = kotlinTargetVersion
-            jvmTarget = javaTargetVersion
-        }
-    }
+	withType<KotlinCompile>().configureEach {
+		kotlinOptions {
+			languageVersion = kotlinTargetVersion
+			apiVersion = kotlinTargetVersion
+			jvmTarget = javaTargetVersion
+		}
+	}
 
-    // Per https://github.com/gradle/gradle/issues/17271, Kotlin modules only
-    // compile if the Kotlin compiler destination directory is set to that of
-    // Java compiler.
-    named<KotlinCompile>("compileKotlin").configure {
-        val compileJava by getting(JavaCompile::class)
-        destinationDirectory.set(compileJava.destinationDirectory)
-    }
+	// Per https://github.com/gradle/gradle/issues/17271, Kotlin modules only
+	// compile if the Kotlin compiler destination directory is set to that of
+	// Java compiler.
+	named<KotlinCompile>("compileKotlin").configure {
+		val compileJava by getting(JavaCompile::class)
+		destinationDirectory.set(compileJava.destinationDirectory)
+	}
 
-    withType<Test>().configureEach {
-        useJUnitPlatform()
-    }
+	withType<Test>().configureEach {
+		useJUnitPlatform()
+	}
 
-    named<Wrapper>("wrapper").configure {
-        gradleVersion = "7.6"
-        distributionType = ALL
-    }
+	named<Wrapper>("wrapper").configure {
+		gradleVersion = "7.6"
+		distributionType = ALL
+	}
 }
