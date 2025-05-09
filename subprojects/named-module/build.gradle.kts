8cf053fc6645a2f9261f3407ac7b428699167df2
Ian Brandt <ian@ianbrandt.com>
2025-05-09 06:03:21 -0700

    Refactored JPMS argument handling and referenced https://youtrack.jetbrains.com/issue/KT-20740 blocker.

--- subprojects/named-module/build.gradle.kts
+++ subprojects/named-module/build.gradle.kts
@@ -1,14 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
 	application
 	id("com.ianbrandt.buildlogic.kotlin-project")
 }
 
+val mainModuleName = "com.ianbrandt.jpms.named"
+
 val jpmsArgs = listOf(
-	"--add-reads", "com.ianbrandt.jpms.named=ALL-UNNAMED",
+	"--add-reads", "$mainModuleName=ALL-UNNAMED",
 )
 
+val javaToKotlinJpmsArgMapping = mapOf(
+	"--add-exports" to "-Xadd-exports",
+	"--add-reads" to "-Xadd-reads",
+	"--patch-module" to "-Xpatch-module",
+)
+
+val kotlinJpmsArgs = jpmsArgs.chunked(2).flatMap { (jpmsArg, value) ->
+	val kotlinJpmsArg = (javaToKotlinJpmsArgMapping[jpmsArg]
+		?: error("Unrecognized JPMS argument: $jpmsArg"))
+	listOf(kotlinJpmsArg, value)
+}
+
 application {
-	mainModule.set("com.ianbrandt.jpms.named")
+	mainModule.set(mainModuleName)
 	mainClass.set("com.ianbrandt.jpms.HelloWorldKt")
 	applicationDefaultJvmArgs = jpmsArgs
 }
@@ -24,4 +40,10 @@ tasks {
 			isFork = true
 		}
 	}
+	// Not supported yet: https://youtrack.jetbrains.com/issue/KT-20740
+	//withType<KotlinCompile>().configureEach {
+	//	compilerOptions {
+	//		freeCompilerArgs.addAll(kotlinJpmsArgs)
+	//	}
+	//}
 }
