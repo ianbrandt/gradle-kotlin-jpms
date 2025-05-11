a48f8651e28ff248f10bad7b10c7717e9f353068
Ian Brandt <ian@ianbrandt.com>
2025-05-11 15:29:20 -0700

    Extracted main module value.

--- subprojects/demo/build.gradle.kts
+++ subprojects/demo/build.gradle.kts
@@ -3,19 +3,28 @@ plugins {
 	id("com.ianbrandt.build.kotlin-project")
 }
 
+val mainModuleName = "com.ianbrandt.jpms.demo"
+
 application {
-	mainModule.set("com.ianbrandt.jpms.demo")
-	mainClass.set("com.ianbrandt.jpms.HelloWorld")
+	mainModule.set(mainModuleName)
+	mainClass.set("com.ianbrandt.jpms.HelloWorldKt")
 }
 
 tasks {
-	// Enable JPMS: https://youtrack.jetbrains.com/issue/KT-55389/
+	// Enable JPMS for a module with only Kotlin sources:
+	// https://youtrack.jetbrains.com/issue/KT-55389/
 	named<JavaCompile>("compileJava") {
+		// For Gradle configuration cache compatibility,
+		// `CommandLineArgumentProvider { }` must only capture local variables
+		// of compatible types...
+		// https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:not_yet_implemented:accessing_top_level_at_execution
+		val localMainModuleName = mainModuleName
+		// https://docs.gradle.org/current/userguide/configuration_cache.html#gradle_model_types
 		val mainSourceSetOutput: FileCollection = sourceSets["main"].output
 		options.compilerArgumentProviders.add(CommandLineArgumentProvider {
 			listOf(
 				"--patch-module",
-				"com.ianbrandt.jpms.demo=${mainSourceSetOutput.asPath}"
+				"$localMainModuleName=${mainSourceSetOutput.asPath}"
 			)
 		})
 	}
