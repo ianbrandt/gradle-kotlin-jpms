bb883f24a05a7ba81011bec45cc61d797f576cf0
Ian Brandt <ian@ianbrandt.com>
2025-05-11 15:34:34 -0700

    Removed empty Java package configuration and workaround class as it no longer seems to be needed to build and run a Kotlin-only module.

--- subprojects/demo/build.gradle.kts
+++ subprojects/demo/build.gradle.kts
@@ -9,23 +9,3 @@ application {
 	mainModule.set(mainModuleName)
 	mainClass.set("com.ianbrandt.jpms.HelloWorldKt")
 }
-
-tasks {
-	// Enable JPMS for a module with only Kotlin sources:
-	// https://youtrack.jetbrains.com/issue/KT-55389/
-	named<JavaCompile>("compileJava") {
-		// For Gradle configuration cache compatibility,
-		// `CommandLineArgumentProvider { }` must only capture local variables
-		// of compatible types...
-		// https://docs.gradle.org/current/userguide/configuration_cache.html#config_cache:not_yet_implemented:accessing_top_level_at_execution
-		val localMainModuleName = mainModuleName
-		// https://docs.gradle.org/current/userguide/configuration_cache.html#gradle_model_types
-		val mainSourceSetOutput: FileCollection = sourceSets["main"].output
-		options.compilerArgumentProviders.add(CommandLineArgumentProvider {
-			listOf(
-				"--patch-module",
-				"$localMainModuleName=${mainSourceSetOutput.asPath}"
-			)
-		})
-	}
-}
--- subprojects/demo/src/main/java/com/ianbrandt/jpms/JpmsPackageEmptyWorkaround.java
+++
@@ -1,4 +0,0 @@
-package com.ianbrandt.jpms;
-
-public class JpmsPackageEmptyWorkaround {
-}
